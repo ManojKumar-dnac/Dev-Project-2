fc274c7f65792c5bc7d697895cff785da8571a71
Manoj <karanaku.cisco.com>
2026-04-19 18:43:46 +0530

    Adding new file

---
+++ Developer/Dev-2.py
@@ -0,0 +1,10 @@
+import netmiko
+import netmiko from ConnectionHandler
+
+dev = {
+         username = '$username'
+	 password = '$password'
+	}
+
+Connection = ConnectionHandler.connect(dev)
+print  "The connecion happend  " Connection
